c03aca131a08b16fc2c9dc04417a2f77c2959b16
zhangxingzhong <744719042@qq.com>
2019-06-05 14:30:35 +0800

    初始化base类库

--- base/src/main/java/com/example/base/pulltorefresh/base/BaseRecyclerAdapter.java
+++
@@ -1,7 +0,0 @@
-package com.example.base.pulltorefresh.base;
-
-import android.support.v7.widget.RecyclerView;
-
-public abstract class BaseRecyclerAdapter<VH extends BaseRecyclerViewHolder> extends RecyclerView.Adapter<VH> {
-
-}
