4641af19e20fc5c0faffe9d4758e122b099ac3e3
zhangxingzhong <744719042@qq.com>
2019-06-05 16:55:16 +0800

    添加自定义控件代码

---
+++ base/src/main/java/com/example/base/widget/custom/FiveCellLayoutManager.java
@@ -0,0 +1,58 @@
+package com.example.base.widget.custom;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.example.mycustomwidget.R;
+
+public class FiveCellLayoutManager extends BaseLayoutManager {
+    @Override
+    public int onMeasure(int width, Context context, AttributeSet attrs) {
+        if (attrs != null) {
+            TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.AbNormalLayout);
+            mAspect = array.getFloat(R.styleable.AbNormalLayout_fiveCardsAspect, mAspect);
+            array.recycle();
+        } else {
+
+        }
+        return (int) (width * mAspect);
+    }
+
+    @Override
+    public void onLayout(AbNormalLayout layout, int cellPadding) {
+        int paddingTop = layout.getPaddingTop();
+        int paddingLeft = layout.getPaddingLeft();
+
+        View view = layout.getChildAt(0);
+        int x = paddingLeft, y = paddingTop;
+        view.layout(x, y, x + view.getMeasuredWidth(), y + view.getMeasuredHeight());
+
+        x = x + view.getMeasuredWidth() + cellPadding;
+        view = layout.getChildAt(1);
+        view.layout(x, y, x + view.getMeasuredWidth(), y + view.getMeasuredHeight());
+
+        x = x + view.getMeasuredWidth() + cellPadding;
+        view = layout.getChildAt(2);
+        view.layout(x, y, x + view.getMeasuredWidth(), y + view.getMeasuredHeight());
+
+        x = x - view.getMeasuredWidth() - cellPadding;
+        y = y + view.getMeasuredHeight() + cellPadding;
+        view = layout.getChildAt(3);
+        view.layout(x, y, x + view.getMeasuredWidth(), y + view.getMeasuredHeight());
+
+        x = x + view.getMeasuredWidth() + cellPadding;
+        view = layout.getChildAt(4);
+        view.layout(x, y, x + view.getMeasuredWidth(), y + view.getMeasuredHeight());
+    }
+
+    @Override
+    public void measureChildView(AbNormalLayout parent, View view, int cellPadding) {
+        int width = parent.getMeasuredWidth();
+        int height = parent.getMeasuredHeight();
+        width -= 2 * cellPadding + parent.getPaddingLeft() + parent.getPaddingRight();
+        height -= parent.getPaddingBottom() + parent.getPaddingTop() - cellPadding;
+        measure(view, width, height);
+    }
+}
---
+++ base/src/main/java/com/example/base/widget/custom/ILayoutManager.java
@@ -0,0 +1,11 @@
+package com.example.base.widget.custom;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public interface ILayoutManager {
+    int onMeasure(int width, Context context, AttributeSet attrs);
+    void onLayout(AbNormalLayout layout, int cellPadding);
+    void measureChildView(AbNormalLayout parent, View view, int cellPadding);
+}
